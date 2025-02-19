883aa7efb5b2bf60b7d8cb12e58df3f89ea8b817
baoshi.rao <baoshi.rao@ucloud.cn>
2025-02-19 18:22:29 +0800

    refactor(examples): 更新 isms 示例代码
    - 添加了发送视频短信的功能
    - 新增了读取配置文件和生成签名的代码
    - 修改了主函数的结构，增加了测试发送消息的函数

--- examples/isms/main.py
+++ examples/isms/main.py
@@ -1,1 +1,38 @@
-11
+import json
+
+from ucloud.client import Client
+from ucloud.core import auth
+
+with open('./config.json', 'r') as fp:
+    client_config = json.load(fp)
+
+client = Client(
+    client_config
+)
+
+
+def get_signature(data: dict):
+    cred = auth.Credential(
+        "4ZF9o0XPisb6zXaLguqDl18mG56XvnGDx",
+        "1s6wQQ9yhuJ4MF2TwwGQlPAZpDzzilMrFYwHOOIT0jZ",
+    )
+    return cred.verify_ac(data)
+
+
+def test_send_message():
+    # 发送视频短信
+    # response= client.uaccount().get_project_list()
+    # print(response)
+    iclient = client.isms()
+    iclient.config.region = None
+    response = iclient.send_isms_message({
+        "TemplateId": "UVTG250218EEE310",
+        "PhoneSet": ["15755407860"],
+        "Zone": "",
+        "Region": ""
+    })
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
